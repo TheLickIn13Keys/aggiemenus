c3b55cff6cd6f9197fee69f8f65c83e01133411b
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2024-11-13 18:15:03 -0800

    updated card UI

--- frontend/app/menu/FoodItemCard.tsx
+++ frontend/app/menu/FoodItemCard.tsx
@@ -13,46 +13,47 @@ im adding some custom colors in the tailwind.config.js file for the new filters,
 const FoodItemCard = ({ foodItem, index }: Props) => {
   return (
     <label htmlFor={`food_item_${index}`}>
-      <div className="flex flex-row justify-between items-center rounded bg-white px-6 py-4 hover:bg-slate-200 hover:cursor-pointer transition duration-300 ease-in-out">
-        <div className="flex flex-col">
-          <div className="font-semibold text-sm text-textDarkBlue">
-            {foodItem.common_items.name}
+      <div className="flex items-center rounded bg-white hover:bg-slate-200 hover:cursor-pointer transition duration-300 ease-in-out">
+        <div className="w-full p-[20px]">  {/* content container for each food item card */}
+          <div className="flex flex-col">  {/* sub container for just item title and calorie */}
+            <div className="font-semibold text-sm text-textDarkBlue">
+              {foodItem.common_items.name}
+            </div>
+            {foodItem.common_items.calories !== "N/A" && (
+              <div className="font-red-hat font-normal text-[11px] text-calorieGray sm:max-w-fit sm:px-3 sm:py-1 sm:rounded-full sm:bg-calorieBlue sm:text-white">
+                {Math.round(parseInt(foodItem.common_items.calories))} cal
+              </div>
+            )}
           </div>
-          <div className="text-gray-400 text-xs">
-            {foodItem.section}
+
+
+          <div className="mt-[8px] flex flex-row gap-[7px] items-center"> {/* sub container for food filter tags */}
+
+            {/* Vegan */}
+            {foodItem.common_items.vegan && (
+              <div className="text-white px-[8px] py-[5px] rounded-full bg-veganGreen text-xs font-red-hat">
+                Vegan
+              </div>
+            )}
+            {/* Vegetarian */}
+            {foodItem.common_items.vegetarian && (
+              <div className="text-white px-[8px] py-[5px] rounded-full bg-vegetarianOrange text-xs font-red-hat">
+                Vegetarian
+              </div>
+            )}
+            {/* Halal */}
+            {foodItem.common_items.halal && (
+              <div className="text-white px-[8px] py-[5px] rounded-full bg-halalPink text-xs font-red-hat">
+                Halal
+              </div>
+            )}
+            {/* pescetarian */}
+            {foodItem.common_items.pescetarian && (
+            <div className="text-white px-[8px] py-[5px] rounded-full bg-pescetarianYellow text-xs font-red-hat">
+              Pescetarian
+            </div>
+          )}
           </div>
-        </div>
-        <div className="flex flex-row gap-2 items-center">
-          {/* Calories */}
-          {foodItem.common_items.calories !== "N/A" && (
-            <div className="px-3 py-1 rounded-full bg-calorieBlue text-white text-xs font-red-hat">
-              {Math.round(parseInt(foodItem.common_items.calories))} cal
-            </div>
-          )}
-          {/* Vegan */}
-          {foodItem.common_items.vegan && (
-            <div className="px-[8px] py-[5px] rounded-full bg-veganGreen text-xs font-red-hat">
-              Vegan
-            </div>
-          )}
-          {/* Vegetarian */}
-          {foodItem.common_items.vegetarian && (
-            <div className="px-[8px] py-[5px] rounded-full bg-vegetarianOrange text-xs font-red-hat">
-              Vegetarian
-            </div>
-          )}
-          {/* Halal */}
-          {foodItem.common_items.halal && (
-            <div className="px-[8px] py-[5px] rounded-full bg-halalPink text-xs font-red-hat">
-              Halal
-            </div>
-          )}
-          {/* pescetarian */}
-          {foodItem.common_items.pescetarian && (
-          <div className="px-[8px] py-[5px] rounded-full bg-pescetarianYellow text-xs font-red-hat">
-            Pescetarian
-          </div>
-          )}
         </div>
       </div>
     </label>
