86c37926fdc18433f8f19969195308e0bf1035c4
patrickhcnguyen <patricknguyen714@gmail.com>
2025-02-08 15:10:54 -0800

    added selectors

--- frontend/app/favorites/mobile/page.tsx
+++ frontend/app/favorites/mobile/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useFavoritesStore } from '../util/favoritesStore';
 import { MdKeyboardArrowLeft } from "react-icons/md";
@@ -7,12 +7,15 @@ import { IoChevronBackOutline } from "react-icons/io5";
 
 // layout:
   // header
+    // done
   // switch between offered this week and available now (can reuse from desktop component)
+    // done 
   // list of favorites component
-  // can reuse a lot of the card/code from the desktop component 
+    
 
 const MobileFavoritesPage = () => {
   const { favorites } = useFavoritesStore();
+  const [activeTab, setActiveTab] = useState<'available' | 'all'>('available');
 
   return (
     // bg
@@ -24,9 +27,26 @@ const MobileFavoritesPage = () => {
             <IoChevronBackOutline size={20} className="text-primary hover:text-opacity-80" />
           </button>
         </Link>
-        <h4 className='w-full text-center text-primary font-semibold'>
+        <h4 className='w-full text-center text-[14px] text-textDarkBlue font-semibold'>
           Favorited Items
         </h4>
+      </div>
+      {/* list of favorites component */}
+      <div className='px-[40px] border-b border-[#C3D9ED] flex justify-center'>
+        <div className='flex gap-x-28 font-red-hat text-textDarkBlue text-[14px] font-bold justify-center'>
+          <button
+            onClick={() => setActiveTab('available')}
+            className={`pt-[20px] pb-[20px] ${activeTab === 'available' ? 'pt-[20px] pb-[20px] text-primary border-b-2 border-primary' : ''}`}
+          >
+            Offered this week
+          </button>
+          <button
+            onClick={() => setActiveTab('all')}
+            className={`pt-[20px] pb-[20px] ${activeTab === 'all' ? 'pt-[20px] pb-[20px] text-primary border-b-2 border-primary' : ''}`}
+          >
+            All favorited items
+          </button>
+        </div>
       </div>
     </div>
   );
